fedd03bdf3bd5b09b7ab758794776a6350c1b4ff
Maria <136898835+Mvlkr@users.noreply.github.com>
2023-12-04 14:35:55 +0300

    Update main.py

--- new_datasets_package/main.py
+++ new_datasets_package/main.py
@@ -8,8 +8,9 @@ from pathlib import Path
 from sklearn.model_selection import train_test_split
 
 from m_package.models.CE_GAN import build_generator, build_discriminator, GAN, class_expert_model
+from m_package.models.CE_GAN_LSTM import build_discriminator_lstm, class_expert_model_lstm
 from m_package.models.Conv_LSTM_grad import build_basic, build_deep, ConvLSTM
-from m_package.models.ResNet import Resnet
+from m_package.models.ResNet import Resnet, Resnet_LSTM
 from m_package.data.creartion import DyslexiaVizualization
 from m_package.common.metrics_multi import metrics_per_fold, resulting
 from m_package.common.metrics_binary import metrics_per_fold_binary, resulting_binary
@@ -22,8 +23,9 @@ def args_parser(arguments):
     _data_name = arguments.data_name.lower()
     _model_name = arguments.model_name.lower()
     _num_classes = arguments.num_classes
+    _type_name = arguments.model_name.lower()
 
-    return  _run, _epoch_num, _data_name, _model_name, _num_classes
+    return  _run, _epoch_num, _data_name, _model_name, _num_classes, _type_name
 
 if __name__ == "__main__":
 
@@ -67,9 +69,16 @@ if __name__ == "__main__":
             " 3 is for multi-class"
     )
 
+    parser.add_argument(
+         "--type_name", type=str, default="conv",
+        help="Model's name"
+            "conv is for convolutional type"
+            "lstm is for convlstm type"
+    )
+
     args = parser.parse_args()
 
-    run, epoch_num, data_name, model_name, num_classes = args_parser(arguments=args)
+    run, epoch_num, data_name, model_name, num_classes, type_name = args_parser(arguments=args)
     batch_size = 16
 
     print(
@@ -78,7 +87,8 @@ if __name__ == "__main__":
         "  data_name:", data_name, "\n",
         "  run:", run, "\n",
         "  epochs:", epoch_num,"\n",
-        "  num_classes", num_classes, "\n"
+        "  num_classes", num_classes, "\n",
+        "  type", type_name, "\n"
     )
 
     path = Path("Datasets")
@@ -189,6 +199,49 @@ if __name__ == "__main__":
 
         return train_dataset_norm, train_dataset_dys
     
+    def train_GAN(save:bool = True):
+        if type_name == "conv":
+            build_discriminator_func = build_discriminator
+        elif type_name == "lstm":
+            build_discriminator_func = build_discriminator_lstm
+        #data for gan model
+        norm_dataset, dys_dataset = GAN_data(data_name)
+
+        image_shape = size + [1]
+        gan_epoch = 200
+        #build and train generator for norm class
+        generator_norm = build_generator(image_shape=tuple(image_shape), dense_image_shape=np.prod(image_shape))
+        discriminator_norm = build_discriminator_func(size)
+        gan_norm = GAN(generator_norm, discriminator_norm, batch_size=batch_size)
+        gan_norm.compile(g_opt=tf.keras.optimizers.Adam(1e-4),
+                             d_opt=tf.keras.optimizers.Adam(1e-4),
+                             g_loss=tf.keras.losses.BinaryCrossentropy(),
+                             d_loss=tf.keras.losses.BinaryCrossentropy())
+
+        path = "Figures"
+        model_name_save_n = f"{gan_epoch}{data_name}_GAN_norm_{num_classes}"
+        hist_norm = gan_norm.fit(norm_dataset, epochs=gan_epoch)
+        GAN_plot(hist_norm, path, model_name_save_n)
+        if save:
+            save_model(generator_norm, model_name_save_n)
+        
+        #build and train generator for dys class
+        generator_dys = build_generator(image_shape=tuple(image_shape), dense_image_shape=np.prod(image_shape))
+        discriminator_dys = build_discriminator_func(size)
+        gan_dys = GAN(generator_dys, discriminator_dys, batch_size=batch_size)
+        gan_dys.compile(g_opt=tf.keras.optimizers.Adam(1e-4),
+                             d_opt=tf.keras.optimizers.Adam(1e-4),
+                             g_loss=tf.keras.losses.BinaryCrossentropy(),
+                             d_loss=tf.keras.losses.BinaryCrossentropy())
+
+        model_name_save_d = f"{gan_epoch}{data_name}_GAN_dys_{num_classes}"
+        hist_dys = gan_dys.fit(dys_dataset, epochs=gan_epoch)
+        GAN_plot(hist_dys, path, model_name_save_d)
+        if save:
+            save_model(generator_dys, model_name_save_d)
+
+        return discriminator_norm, discriminator_dys
+    
 
     def build(n_classes, model_n):
         if n_classes == 3:
@@ -297,14 +350,20 @@ if __name__ == "__main__":
     
     
     elif model_name == "resnet" and num_classes == 2:
+
+        if type_name == "conv":
+            func_model = Resnet
+        elif type_name == "lstm":
+            func_model = Resnet_LSTM
+
         if run == 1: #done
             train_dataset, val_dataset, test_dataset = split_data(X_data, y_data) 
-            model = Resnet(tuple(size + [1]))
+            model = func_model(tuple(size + [1]))
             model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4), loss="binary_crossentropy", metrics=tf.keras.metrics.AUC())
             history = model.fit(train_dataset, validation_data=(val_dataset), epochs=epoch_num)
 
             path = "Figures"
-            model_name_save = f"{epoch_num}{data_name}_{model_name}_{num_classes}"
+            model_name_save = f"{epoch_num}{data_name}_{model_name}_{num_classes}_{type_name}"
 
             plot_history(history.history['loss'], history.history['val_auc'], history.history['auc'], path, model_name_save, history.history['val_loss'])
             plot_loss(history.history['loss'], path, model_name_save)
@@ -322,7 +381,7 @@ if __name__ == "__main__":
                 #creating the datasets
                 train_dataset, val_dataset, test_dataset = split_data(X_data, y_data)
                 #build and train model on huge number of epochs
-                model = Resnet(tuple(size + [1]))
+                model = func_model(tuple(size + [1]))
                 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4), loss="binary_crossentropy", metrics=tf.keras.metrics.AUC())
                 model.fit(train_dataset, validation_data=(val_dataset), epochs=epoch_num)
 
@@ -333,7 +392,7 @@ if __name__ == "__main__":
             final_results = resulting_binary(metrics_results)
             print(final_results)
 
-            model_name_save = f"{epoch_num}{data_name}_{model_name}_{num_classes}"
+            model_name_save = f"{epoch_num}{data_name}_{model_name}_{num_classes}_{type_name}"
 
             saving_results(final_results, model_name_save)
             save_model(model, f"model_{model_name_save}")
@@ -342,45 +401,19 @@ if __name__ == "__main__":
             conf_matrix(model, test_dataset, f"model_{model_name_save}")
 
     elif model_name == "ce_gan" and num_classes == 2:
+        if type_name == "conv":
+            ce_model_func = class_expert_model
+        elif type_name == "lstm":
+            ce_model_func_func = class_expert_model_lstm
+    
         if run == 1: #train gan and plot amount of epoch for ce model 
             #data for ce_model
             train_dataset, val_dataset, test_dataset = split_data(X_data, y_data)
-
-            #data for gan model
-            norm_dataset, dys_dataset = GAN_data(data_name)
-
+            discriminator_norm, discriminator_dys = train_GAN()
             image_shape = size + [1]
-            gan_epoch = 200
-            #build and train generator for norm class
-            generator_norm = build_generator(image_shape=tuple(image_shape), dense_image_shape=np.prod(image_shape))
-            discriminator_norm = build_discriminator(size)
-            gan_norm = GAN(generator_norm, discriminator_norm, batch_size=batch_size)
-            gan_norm.compile(g_opt=tf.keras.optimizers.Adam(1e-4),
-                             d_opt=tf.keras.optimizers.Adam(1e-4),
-                             g_loss=tf.keras.losses.BinaryCrossentropy(),
-                             d_loss=tf.keras.losses.BinaryCrossentropy())
-
-            path = "Figures"
-            model_name_save_n = f"{gan_epoch}{data_name}_GAN_norm_{num_classes}"
-            hist_norm = gan_norm.fit(norm_dataset, epochs=gan_epoch)
-            GAN_plot(hist_norm, path, model_name_save_n)
-        
-            #build and train generator for dys class
-            generator_dys = build_generator(image_shape=tuple(image_shape), dense_image_shape=np.prod(image_shape))
-            discriminator_dys = build_discriminator(size)
-            gan_dys = GAN(generator_dys, discriminator_dys, batch_size=batch_size)
-            gan_dys.compile(g_opt=tf.keras.optimizers.Adam(1e-4),
-                             d_opt=tf.keras.optimizers.Adam(1e-4),
-                             g_loss=tf.keras.losses.BinaryCrossentropy(),
-                             d_loss=tf.keras.losses.BinaryCrossentropy())
-
-            model_name_save_d = f"{gan_epoch}{data_name}_GAN_dys_{num_classes}"
-            hist_dys = gan_dys.fit(dys_dataset, epochs=gan_epoch)
-            GAN_plot(hist_dys, path, model_name_save_d)
-
             #build and train ce_model
             weights_ce = [discriminator_norm.layers[6].get_weights(), discriminator_dys.layers[6].get_weights()]
-            ce_model = class_expert_model(weights_ce, tuple(image_shape))
+            ce_model = ce_model_func(weights_ce, tuple(image_shape))
 
             ce_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4), loss="binary_crossentropy", metrics=tf.keras.metrics.AUC())
             history = ce_model.fit(train_dataset, validation_data=(val_dataset), epochs=epoch_num)
@@ -392,5 +425,39 @@ if __name__ == "__main__":
             plot_loss(history.history['loss'], path, model_name_save)
 
         elif run == 2:
-            pass
+            discriminator_norm, discriminator_dys = train_GAN()
+            image_shape = size + [1]
+            #build and train ce_model
+            weights_ce = [discriminator_norm.layers[6].get_weights(), discriminator_dys.layers[6].get_weights()]
 
+            # before training params
+            metrics_results = {
+                "auc_roc" : [],
+                "accuracy" : [],
+                "precision": [],
+                "recall": [],
+                "f1": []
+            }
+
+            for _ in range(5):
+                #creating the datasets
+                train_dataset, val_dataset, test_dataset = split_data(X_data, y_data)
+                #build and train model on huge number of epochs
+                ce_model = ce_model_func(weights_ce, tuple(image_shape))
+                ce_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4), loss="binary_crossentropy", metrics=tf.keras.metrics.AUC())
+                ce_model.fit(train_dataset, validation_data=(val_dataset), epochs=epoch_num)
+
+                #calc metrics 
+                metrics_results = metrics_per_fold_binary(ce_model, test_dataset, metrics_results)
+                print(metrics_results)
+
+            final_results = resulting_binary(metrics_results)
+            print(final_results)
+
+            model_name_save = f"{epoch_num}{data_name}_{model_name}_{num_classes}"
+
+            saving_results(final_results, model_name_save)
+            save_model(ce_model, f"model_{model_name_save}")
+            
+            #saving conf_matrix
+            conf_matrix(ce_model, test_dataset, f"model_{model_name_save}")

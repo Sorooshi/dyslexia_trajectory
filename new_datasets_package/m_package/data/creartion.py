b15890d9fe6f09cd72cdad0f99c688e0580aa14b
Maria <136898835+Mvlkr@users.noreply.github.com>
2024-01-19 20:39:18 +0300

    Update creartion.py data types

--- new_datasets_package/m_package/data/creartion.py
+++ new_datasets_package/m_package/data/creartion.py
@@ -6,6 +6,91 @@ import os
 from pathlib import Path
 from sklearn import preprocessing
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import io
+
+
+def draw_scatter(X, Y, dur):
+    fig, ax = plt.subplots(figsize=(18, 6), dpi=100)
+    fig.patch.set_facecolor('black')
+    ax.set_axis_off()
+    ax.patch.set_facecolor('white')
+    ax.patch.set_alpha(0.0)
+    ax.invert_yaxis()
+    plt.scatter(X, Y, c=dur, s=dur, cmap='gray', vmin=0)
+    return fig
+
+def fig_to_np(fig):
+    buf = io.BytesIO()
+    fig.savefig(buf, format='png', bbox_inches='tight')
+    buf.seek(0)
+    img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
+    buf.close()
+    img = cv.imdecode(img_arr, cv.IMREAD_GRAYSCALE)
+    img = cv.resize(img, (180, 60), interpolation=cv.INTER_LINEAR)
+    img[0, :] = img[img.shape[0] - 1, :] = img[:, 0] = img[:, img.shape[1] - 1] = 0
+    plt.close(fig)
+    return img
+
+def img_dataset_creation(path, dataset_name):
+    fixation = os.path.join(path, dataset_name)
+    fix_data = pd.read_csv(fixation)
+
+    x_train_img, y_train_img = [], []
+
+    for ids in fix_data["SubjectID"].unique():
+        X = fix_data[fix_data["SubjectID"] == ids]["FIX_X"]
+        Y = fix_data[fix_data["SubjectID"] == ids]["FIX_Y"]
+        dur = fix_data[fix_data["SubjectID"] == ids]["FIX_DURATION"]
+
+        img = draw_scatter(X, Y, dur)
+        np_img = fig_to_np(img)
+        x_train_img.append(np_img)
+        y_train_img.append(fix_data[fix_data["SubjectID"] == ids]["Group"].values[0] - 1)
+
+    x_train_img = np.array(x_train_img)
+    y_train_img = tf.keras.utils.to_categorical(y_train_img)
+    y_train_img = np.array(y_train_img)
+
+    return x_train_img, y_train_img
+
+
+def splitting_window(features, targets, n_steps):
+    x_seq, y_seq = [], []
+    for i in range(len(features)):
+        end_idx = i + n_steps
+        if end_idx >= len(features):
+            break
+        x_seq.append(features[i:end_idx, :])
+        y_seq.append(targets[end_idx])
+
+    return np.asarray(x_seq), np.asarray(y_seq)
+
+def window_dataset_creation(n_steps, path, dataset_name):
+    fixation = os.path.join(path, dataset_name)
+    data = pd.read_csv(fixation)
+
+    df_id = data[data['SubjectID'] == data["SubjectID"].unique()[0]]
+    y_train = df_id.loc[:, ["Group"]]
+    x_train = df_id.loc[:, ["FIX_X", "FIX_Y", "FIX_DURATION"]]
+    x_train_windowed_final, y_train_windowed_final = splitting_window(x_train.values, y_train.values, n_steps)
+
+
+    for ids in data["SubjectID"].unique()[1:]:
+        df_id = data[data['SubjectID'] == ids]
+        y_train = df_id.loc[:, ["Group"]]
+        x_train = df_id.loc[:, ["FIX_X", "FIX_Y", "FIX_DURATION"]]
+        x_train_windowed, y_train_windowed = splitting_window(x_train.values, y_train.values, n_steps)
+        x_train_windowed_final = np.concatenate((x_train_windowed_final, x_train_windowed))
+        y_train_windowed_final = np.concatenate((y_train_windowed_final, y_train_windowed))
+
+
+    y_train_windowed_final = y_train_windowed_final.reshape(-1) - 1
+    y_train_windowed_final  = tf.keras.utils.to_categorical(y_train_windowed_final )
+    y_train_windowed_final  = np.array(y_train_windowed_final)
+
+    return x_train_windowed_final, y_train_windowed_final
+
 
 class DyslexiaVizualization:
     def __init__(self, shape, sheet_name: list = [0,1,2], dataset_name: str = "Fixation_report.xlsx", path: Path= Path("../Datasets"), file_format: str = "xlsx") -> None:

c5b680f21604530f616c5cb43aa409d59096fcb6
Maria <136898835+Mvlkr@users.noreply.github.com>
2024-02-23 14:29:23 +0300

    Add files via upload

---
+++ new_datasets_package/m_package/models/basic.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+import keras
+from keras.layers import *
+
+
+def conv_2d_basic(hp):
+    model = keras.Sequential()
+    model.add(Conv2D(filters=hp.Int('filters_1', min_value=32, max_value=128, step=32), 
+                     kernel_size=(hp.Choice('kernel_size_1', values=[5, 7])), 
+                     activation='relu', 
+                     input_shape=(60, 180, 1)))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(filters=hp.Int('filters_2', min_value=64, max_value=256, step=32), 
+                     kernel_size=(hp.Choice('kernel_size_2', values=[3, 5])), 
+                     activation='relu')) 
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(BatchNormalization())
+    model.add(Flatten())
+    model.add(Dense(2, activation='sigmoid'))
+
+
+    learning_rate = hp.Float("lr", min_value=1e-5, max_value=1e-2, sampling="log")
+    
+    optimizer_name = hp.Choice('optimizer', values=['adam', 'rmsprop', 'sgd'])
+    if optimizer_name == 'adam':
+        optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    elif optimizer_name == 'rmsprop':
+        optimizer = keras.optimizers.RMSprop(learning_rate=learning_rate)
+    else:
+        optimizer = keras.optimizers.SGD(learning_rate=learning_rate)
+    
+    model.compile(
+        optimizer=optimizer,
+        loss="binary_crossentropy",
+        metrics=[tf.keras.metrics.AUC(name='auc')],
+    )
+    return model
+
+
+def lstm_2d_basic(hp):
+    model = keras.Sequential()
+    model.add(LSTM(units=hp.Int('lstm1_units', min_value=32, max_value=128, step=32), 
+                   return_sequences=True, input_shape=(10, 3)))
+    model.add(BatchNormalization())
+    model.add(LSTM(units=hp.Int('lstm2_units', min_value=64, max_value=256, step=32))) 
+    model.add(BatchNormalization())
+    model.add(Dense(2, activation='sigmoid'))
+
+    learning_rate = hp.Float("lr", min_value=1e-5, max_value=1e-2, sampling="log")
+    
+    optimizer_name = hp.Choice('optimizer', values=['adam', 'rmsprop', 'sgd'])
+    if optimizer_name == 'adam':
+        optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    elif optimizer_name == 'rmsprop':
+        optimizer = keras.optimizers.RMSprop(learning_rate=learning_rate)
+    else:
+        optimizer = keras.optimizers.SGD(learning_rate=learning_rate)
+    
+    model.compile(
+        optimizer=optimizer,
+        loss="binary_crossentropy",
+        metrics=[tf.keras.metrics.AUC(name='auc')],
+    )
+    return model

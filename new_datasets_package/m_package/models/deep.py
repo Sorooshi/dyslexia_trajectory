636e0831bb70a1bc572c4ad00b8fc25bd459dde6
Maria <136898835+Mvlkr@users.noreply.github.com>
2024-03-07 08:21:59 +0300

    Update deep.py conv

--- new_datasets_package/m_package/models/deep.py
+++ new_datasets_package/m_package/models/deep.py
@@ -403,3 +403,58 @@ def convlstm_1d_deep(hp):
     )
     
     return model
+
+def conv_1d_deep(hp):
+    model = keras.Sequential()
+    model.add(Conv1D(filters=hp.Int('filters_1', min_value=16, max_value=64, step=16), 
+                     kernel_size=hp.Choice('kernel_size_1', values=[5, 7]), 
+                     activation='relu', 
+                     input_shape=(10, 3),
+                     padding='same'))
+    model.add(MaxPooling1D(pool_size=2, padding='same'))
+    model.add(BatchNormalization())
+
+    model.add(Conv1D(filters=hp.Int('filters_2', min_value=32, max_value=128, step=32), 
+                     kernel_size=hp.Choice('kernel_size_2', values = [5, 7]), 
+                     activation='relu',
+                     padding='same'))
+    model.add(MaxPooling1D(pool_size=2, padding='same'))
+    model.add(BatchNormalization())
+
+    model.add(Conv1D(filters=hp.Int('filters_3', min_value=64, max_value=128, step=32), 
+                     kernel_size=hp.Choice('kernel_size_3', values = [3, 5]), 
+                     activation='relu',
+                     padding='same')) 
+    model.add(MaxPooling1D(pool_size=2, padding='same'))
+    model.add(BatchNormalization())
+
+    model.add(Conv1D(filters=hp.Int('filters_4', min_value=64, max_value=256, step=32), 
+                     kernel_size=hp.Choice('kernel_size_4', values = [2, 3]), 
+                     activation='relu',
+                     padding='same')) 
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=2, padding='same'))
+
+    model.add(Flatten())
+    model.add(Dense(hp.Int('dense_units', min_value=64, max_value=256, step=64), activation='relu'))
+    model.add(Dropout(hp.Float('dropout_1', min_value=0.1, max_value=0.5, step=0.1)))
+    model.add(Dense(hp.Int('dense_units_2', min_value=32, max_value=128, step=32), activation='relu'))
+    model.add(Dropout(hp.Float('dropout_2', min_value=0.1, max_value=0.5, step=0.1)))
+    model.add(Dense(2, activation='sigmoid'))
+
+    learning_rate = hp.Float("lr", min_value=1e-5, max_value=1e-2, sampling="log")
+    
+    optimizer_name = hp.Choice('optimizer', values=['adam', 'rmsprop', 'sgd'])
+    if optimizer_name == 'adam':
+        optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    elif optimizer_name == 'rmsprop':
+        optimizer = keras.optimizers.RMSprop(learning_rate=learning_rate)
+    else:
+        optimizer = keras.optimizers.SGD(learning_rate=learning_rate)
+    
+    model.compile(
+        optimizer=optimizer,
+        loss="binary_crossentropy",
+        metrics=[tf.keras.metrics.AUC(name='auc')],
+    )
+    return model

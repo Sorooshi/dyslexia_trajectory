c2b6307a8d4a6873c6b71fba47c8691231e05708
Maria <136898835+Mvlkr@users.noreply.github.com>
2024-05-20 14:55:01 +0300

    Update fused_creation.py

--- new_datasets_package/m_package/data/fused_creation.py
+++ new_datasets_package/m_package/data/fused_creation.py
@@ -3,7 +3,40 @@ import pandas as pd
 import os
 import tensorflow as tf
 
+def splitting_window_fused(features, targets, n_steps):
+    x_seq, y_seq = [], []
+    for i in range(len(features)):
+        end_idx = i + n_steps
+        if end_idx >= len(features):
+            break
+        x_seq.append(features[i:end_idx, :])
+        y_seq.append(targets[end_idx])
 
+    return np.asarray(x_seq), np.asarray(y_seq)
+
+def window_dataset_creation_fused(n_steps, path, dataset_name):
+    fixation = os.path.join(path, dataset_name)
+    data = pd.read_csv(fixation)
+
+    df_id = data[data['SubjectID'] == data["SubjectID"].unique()[0]]
+    y_train = df_id.loc[:, ["Group"]]
+    x_train = df_id.loc[:, ["FIX_X", "FIX_Y", "FIX_DURATION", "Age"]]
+    x_train_windowed_final, y_train_windowed_final = splitting_window_fused(x_train.values, y_train.values, n_steps)
+
+
+    for ids in data["SubjectID"].unique()[1:]:
+        df_id = data[data['SubjectID'] == ids]
+        y_train = df_id.loc[:, ["Group"]]
+        x_train = df_id.loc[:, ["FIX_X", "FIX_Y", "FIX_DURATION", "Age"]]
+        x_train_windowed, y_train_windowed = splitting_window_fused(x_train.values, y_train.values, n_steps)
+        x_train_windowed_final = np.concatenate((x_train_windowed_final, x_train_windowed))
+        y_train_windowed_final = np.concatenate((y_train_windowed_final, y_train_windowed))
+
+    y_train_windowed_final = y_train_windowed_final.reshape(-1) - 1
+    y_train_windowed_final  = tf.keras.utils.to_categorical(y_train_windowed_final )
+    y_train_windowed_final  = np.array(y_train_windowed_final)
+
+    return x_train_windowed_final, y_train_windowed_final
 
 def splitting_window(features, targets, ages, n_steps):
     x_seq, y_seq, age_seq = [], [], []
